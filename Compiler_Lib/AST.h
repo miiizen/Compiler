94df1713a6e4ac904ced46f5ac5a3c9804b8eea2
miiizen <jamesmmizen@gmail.com>
2018-11-25 13:22:54 +0000

    Implement visitor pattern for AST.  Run into a wall with return values and types, save state in visitor class? we can't return values as the accept method gets in the way.

--- Compiler_Lib/AST.h
+++ Compiler_Lib/AST.h
@@ -35,35 +35,50 @@ namespace Compiler {
 	{
 	public:
 		virtual ~AST() = default;
-		virtual ASTType getType() = 0;
+		virtual const ASTType getType() = 0;
+		// Hook into visitor class
+		virtual void accept(Visitor &v) = 0;
 	};
 
 	// Represents a block with an arbitrary number of children
 	class BlockAST : public AST {
-		std::vector<std::shared_ptr<AST>> children;
 		ASTType type = ASTType::BLOCK;
+		std::vector<std::shared_ptr<AST>> children;
 	public:
 		BlockAST(std::vector<std::shared_ptr<AST>> children)
 			: children(std::move(children)) {}
-		virtual ASTType getType() { return type; };
+		virtual const ASTType getType() { return type; };
+		std::vector<std::shared_ptr<AST>> getChildren() { return children; };
+
+		// Visitor hook
+		virtual void accept(Visitor &v) { v.visit(unique_ptr<BlockAST>(this)); };
 	};
 
 	// Represents numeric literals.  Everything is a double at the moment??????
 	class NumberAST : public AST {
-		double val;
 		ASTType type = ASTType::NUMBER;
+		double val;
 	public:
 		NumberAST(double val) : val(val) {}
-		virtual ASTType getType() { return type; };
+		virtual const ASTType getType() { return type; };
+		const double getVal() { return val; };
+
+		// Visitor hook
+		virtual void accept(Visitor &v) { v.visit(unique_ptr<NumberAST>(this)); };
+
 	};
 
 	// Represents a variable
 	class NameAST : public AST {
 		ASTType type = ASTType::NAME;
-	public:
 		std::string name;
+	public:
 		NameAST(const std::string &name) : name(name) {}
-		virtual ASTType getType() { return type; };
+		virtual const ASTType getType() { return type; };
+		const std::string getName() { return name; };
+
+		// Visitor hook
+		virtual void accept(Visitor &v) { v.visit(unique_ptr<NameAST>(this)); };
 	};
 
 	// Compound type
@@ -71,11 +86,14 @@ namespace Compiler {
 		ASTType type = ASTType::ARRAY;
 		std::unique_ptr<AST> name;
 		//DataType type;
-		std::vector<std::shared_ptr<AST>> values;
 	public:
 		ArrayAST(std::unique_ptr<AST> name, std::vector<std::shared_ptr<AST>> vals)
 			: name(std::move(name)), values(std::move(vals)) {}
-		virtual ASTType getType() { return type; };
+		virtual const ASTType getType() { return type; };
+		std::vector<std::shared_ptr<AST>> values;
+
+		// Visitor hook
+		virtual void accept(Visitor &v) { v.visit(unique_ptr<ArrayAST>(this)); };
 	};
 
 	// Represents an assignment expression
@@ -85,7 +103,10 @@ namespace Compiler {
 	public:
 		AssignmentAST(std::unique_ptr<AST> name, std::unique_ptr<AST> rhs)
 			: name(std::move(name)), rhs(std::move(rhs)) {}
-		virtual ASTType getType() { return type; };
+		virtual const ASTType getType() { return type; };
+
+		// Visitor hook
+		virtual void accept(Visitor &v) { v.visit(unique_ptr<AssignmentAST>(this)); };
 	};
 
 	// Represents a function call
@@ -96,7 +117,10 @@ namespace Compiler {
 	public:
 		FuncCallAST(std::unique_ptr<AST> name, std::vector<std::shared_ptr<AST>> args)
 			: name(std::move(name)), args(std::move(args)) {}
-		virtual ASTType getType() { return type; };
+		virtual const ASTType getType() { return type; };
+
+		// Visitor hook
+		virtual void accept(Visitor &v) { v.visit(unique_ptr<FuncCallAST>(this)); };
 	};
 
 	// Represents binary operators.  Can have 2 children
@@ -108,19 +132,25 @@ namespace Compiler {
 	public:
 		BinaryOpAST(TokenType op, std::unique_ptr<AST> lhs, std::unique_ptr<AST> rhs)
 			: op(op), lhs(std::move(lhs)), rhs(std::move(rhs)) {}
-		virtual ASTType getType() { return type; };
+		virtual const ASTType getType() { return type; };
+
+		// Visitor hook
+		virtual void accept(Visitor &v) { v.visit(unique_ptr<BinaryOpAST>(this)); };
 	};
 
 	// Represents a unary operator
 	class UnaryOpAST : public AST {
-		ASTType type = ASTType::PREFIXOP;
+		ASTType type = ASTType::UNARYOP;
 		TokenType op;
 		std::unique_ptr<AST> operand;
 
 	public:
 		UnaryOpAST(TokenType op, std::unique_ptr<AST> operand)
 			: op(op), operand(std::move(operand)) {}
-		virtual ASTType getType() { return type; };
+		virtual const ASTType getType() { return type; };
+
+		// Visitor hook
+		virtual void accept(Visitor &v) { v.visit(unique_ptr<UnaryOpAST>(this)); };
 	};
 
 	// Represents a ternary operator
@@ -131,7 +161,10 @@ namespace Compiler {
 		TernaryOpAST(std::unique_ptr<AST> condition, std::unique_ptr<AST> thenArm, std::unique_ptr<AST> elseArm)
 			: condition(std::move(condition)), thenArm(std::move(thenArm)), 
 			  elseArm(std::move(elseArm)) {}
-		virtual ASTType getType() { return type; };
+		virtual const ASTType getType() { return type; };
+
+		// Visitor hook
+		virtual void accept(Visitor &v) { v.visit(unique_ptr<TernaryOpAST>(this)); };
 	};
 
 	// If/else statement
@@ -142,7 +175,10 @@ namespace Compiler {
 		IfAST(std::unique_ptr<AST> condition, std::unique_ptr<AST> thenBlock, std::unique_ptr<AST> elseBlock)
 			: condition(std::move(condition)), thenBlock(std::move(thenBlock)), 
 			  elseBlock(std::move(elseBlock)) {}
-		virtual ASTType getType() { return type; };
+		virtual const ASTType getType() { return type; };
+
+		// Visitor hook
+		virtual void accept(Visitor &v) { v.visit(unique_ptr<IfAST>(this)); };
 	};
 
 	class ForAST : public AST {
@@ -158,7 +194,10 @@ namespace Compiler {
 			std::unique_ptr<AST> body)
 			: varName(varName), start(std::move(start)), end(std::move(end)),
 			step(std::move(step)), body(std::move(body)) {}
-		virtual ASTType getType() { return type; };
+		virtual const ASTType getType() { return type; };
+
+		// Visitor hook
+		virtual void accept(Visitor &v) { v.visit(unique_ptr<ForAST>(this)); };
 	};
 }
 
---
+++ Compiler_Lib/interpreter.h
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef __INTERPRETER_H
+#define __INTERPRETER_H
+
+#include "visitor.h"
+#include "AST.h"
+
+namespace Compiler {
+	using std::unique_ptr;
+
+	// 'execute' AST as we traverse each node
+	class Interpreter : public Visitor {
+
+	public:
+		// TODO We want to pass values up and down the nodes as we visit them.  State should be STORED in the visitor
+		virtual void visit(unique_ptr<BlockAST> node);
+		virtual void visit(unique_ptr<NumberAST> node);
+		virtual void visit(unique_ptr<NameAST> node);
+		virtual void visit(unique_ptr<ArrayAST> node);
+		virtual void visit(unique_ptr<AssignmentAST> node);
+		virtual void visit(unique_ptr<FuncCallAST> node);
+		virtual void visit(unique_ptr<BinaryOpAST> node);
+		virtual void visit(unique_ptr<UnaryOpAST> node);
+		virtual void visit(unique_ptr<TernaryOpAST> node);
+		virtual void visit(unique_ptr<IfAST> node);
+		virtual void visit(unique_ptr<ForAST> node);
+	};
+}
+#endif
